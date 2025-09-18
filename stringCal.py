7eb21504d771ca909fbfbca2bcfc4a07c2e02813
Jiteshbodele <bodelejitesh@gmail.com>
2025-09-18 19:14:14 +0530

    test for one number in string

--- stringCal.py
+++ stringCal.py
@@ -1,3 +1,7 @@
 def add(str):
     if len(str)==0:
         return 0
+    
+    elif len(str)==1:
+        return int(str)
+
--- testStringCal.py
+++ testStringCal.py
@@ -6,3 +6,10 @@ class StringCalTestCases(unittest.TestCase):
     def test_empty_string(self):
         res=add("")
         self.assertEqual(res,0)
+
+
+    def test_one_num(self):
+        res=add("1")
+        self.assertEqual(res,1)
+
+    
